81fc75996a2840748a4d52c9a4efba5d0c8b448f
Aaryan-R-S <aaryan20004@iiitd.ac.in>
2021-12-06 11:39:53 +0530

    Added Infinte scroll

--- Tutorials/newsapp/src/components/News.js
+++ Tutorials/newsapp/src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export default class News extends Component {
     constructor(props){
@@ -40,6 +41,7 @@ export default class News extends Component {
         this.updateNews();
     }
     
+    // Prev Btn
     handlePrev = async ()=>{
         // console.log("Prev");
         await this.setState({
@@ -48,6 +50,7 @@ export default class News extends Component {
         this.updateNews();
     }
     
+    // Next btn
     handleNext = async ()=>{
         // console.log("Next");
         await this.setState({
@@ -56,30 +59,54 @@ export default class News extends Component {
         this.updateNews();
     }
 
+    // Infinite Scroll 
+    fetchMoreData = async ()=>{
+        await this.setState({
+            page: this.state.page+1
+        })
+        let apiUrl = `https://newsapi.org/v2/top-headlines?country=${this.state.country}&category=${this.state.category}&apiKey=${this.state.apiKey}&page=${this.state.page}&pageSize=${this.state.pgsz}`;
+        // this.setState({loading:true});
+        let myData = await fetch(apiUrl);
+        let parsedData = await myData.json();
+        // console.log(parsedData);
+        this.setState({
+            loading: false,
+            artcl: this.state.artcl.concat(parsedData.articles), 
+            totalArticles: parsedData.totalResults
+        })
+    }
+
     render() {
         return (
-            <div className="container my-3">
-                <h2 className="text-center">NewsMonkey - Top {
+            <>
+                <h2 className="text-center my-3">NewsMonkey - Top {
                         this.props.category.charAt(0).toUpperCase() + this.props.category.slice(1)
                     } Headlines
                 </h2>
                 {this.state.loading && <Spinner/>}
-                <div className="row">
-                    {this.state.loading===false &&
-                        this.state.artcl.map((element)=>{
-                            return (<div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title?element.title:"Something went wrong"} description={element.description?element.description.slice(0, 149):"Something went wrong"} imgUrl={element.urlToImage?element.urlToImage:"https://images.livemint.com/img/2021/12/03/600x338/415be322-4602-11eb-bc1d-4bfe13e32b0e_1608858841050_1608858879986_1638494350428.jpg"} newsUrl={element.url?element.url:"https://www.indiatoday.in/"} author={element.author?element.author:"Unknown"} date={element.publishedAt? (new Date(element.publishedAt)).toLocaleString():"Recently"} sourceName={element.source.name?element.source.name:"Unknown"}/>
-                            </div>);
-                        })
-                    }
-                </div>
-                {this.state.loading===false &&
+                <InfiniteScroll dataLength={this.state.artcl.length} 
+                    next={this.fetchMoreData} 
+                    hasMore={this.state.artcl.length < this.state.totalArticles} 
+                    loader={<Spinner/>}
+                    >
+                    <div className="container">
+                        <div className="row">
+                            {this.state.artcl.map((element)=>{
+                                return (<div className="col-md-4" key={element.url}>
+                                    <NewsItem title={element.title?element.title:"Something went wrong"} description={element.description?element.description.slice(0, 149):"Something went wrong"} imgUrl={element.urlToImage?element.urlToImage:"https://images.livemint.com/img/2021/12/03/600x338/415be322-4602-11eb-bc1d-4bfe13e32b0e_1608858841050_1608858879986_1638494350428.jpg"} newsUrl={element.url?element.url:"https://www.indiatoday.in/"} author={element.author?element.author:"Unknown"} date={element.publishedAt? (new Date(element.publishedAt)).toLocaleString():"Recently"} sourceName={element.source.name?element.source.name:"Unknown"}/>
+                                </div>);
+                            })}
+                        </div>
+                    </div>
+                </InfiniteScroll>
+                {/* Prev/Next Btn code */}
+                {/* {this.state.loading===false &&
                     <div className="container d-flex justify-content-between">
                         <button type="button" disabled={this.state.page<=1} className="btn btn-dark" onClick={this.handlePrev}>&larr; Previous</button>
                         <button type="button" disabled={this.state.page>=Math.ceil(this.state.totalArticles/this.state.pgsz)} className="btn btn-dark" onClick={this.handleNext}>Next &rarr;</button>
                     </div>
-                }
-            </div>
+                } */}
+            </>
         )
     }
 }
